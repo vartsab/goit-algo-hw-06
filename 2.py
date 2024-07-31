b4a80936c40e5ae8c0ccfbb0a6e7afcd0af4270c
varstab <dan.vartsab@gmail.com>
2024-07-31 00:34:55 -0400

    second-commit

--- 2.py
+++ 2.py
@@ -1,43 +1,139 @@
 import networkx as nx
 from collections import deque
 
-# Створення графа
+# Об'явлення графа
 G = nx.Graph()
 
-# Додавання станцій та ліній
+# Задамо станції та лінії
 stations_R = [
-    "Forest Hills - 71 Av", "67 Av", "63 Dr - Rego Park", "Woodhaven Blvd",
-    "Grand Av - Newtown", "Elmhurst Av", "Jackson Hts - Roosevelt Av", "65 St",
-    "Northern Blvd", "46 St", "Steinway St", "36 St", "Queens Plaza", 
-    "Lexington Av/59 St", "5 Av/59 St", "57 St - 7 Av", "49 St", "Times Sq - 42 St",
-    "34 St - Herald Sq", "28 St", "23 St", "14 St - Union Sq", "8 St - NYU",
-    "Prince St", "Canal St", "City Hall", "Cortlandt St", "Rector St",
-    "Whitehall St - South Ferry", "Court St", "Jay St - MetroTech", 
-    "DeKalb Av", "Atlantic Av - Barclays Ctr", "Union St", "9 St", "Prospect Av",
-    "25 St", "36 St", "45 St", "53 St", "59 St", "Bay Ridge Av", "77 St",
-    "86 St", "Bay Ridge - 95 St"
+    "Forest Hills - 71 Av", 
+    "67 Av", 
+    "63 Dr - Rego Park", 
+    "Woodhaven Blvd",
+    "Grand Av - Newtown", 
+    "Elmhurst Av", 
+    "Jackson Hts - Roosevelt Av", 
+    "65 St",
+    "Northern Blvd", 
+    "46 St", 
+    "Steinway St", 
+    "36 St", 
+    "Queens Plaza", 
+    "Lexington Av/59 St", 
+    "5 Av/59 St", 
+    "57 St - 7 Av", 
+    "49 St", 
+    "Times Sq - 42 St",
+    "34 St - Herald Sq", 
+    "28 St", 
+    "23 St", 
+    "14 St - Union Sq", 
+    "8 St - NYU",
+    "Prince St", 
+    "Canal St", 
+    "City Hall", 
+    "Cortlandt St", 
+    "Rector St",
+    "Whitehall St - South Ferry", 
+    "Court St", 
+    "Jay St - MetroTech", 
+    "DeKalb Av", 
+    "Atlantic Av - Barclays Ctr", 
+    "Union St", 
+    "9 St", 
+    "Prospect Av",
+    "25 St", 
+    "36 St", 
+    "45 St", 
+    "53 St", 
+    "59 St", 
+    "Bay Ridge Av", 
+    "77 St",
+    "86 St", 
+    "Bay Ridge - 95 St"
 ]
 
 stations_N = [
-    "Astoria - Ditmars Blvd", "Astoria Blvd", "30 Av", "Broadway",
-    "36 Av", "39 Av - Dutch Kills", "Queensboro Plaza", "Lexington Av/59 St",
-    "5 Av/59 St", "57 St - 7 Av", "49 St", "Times Sq - 42 St",
-    "34 St - Herald Sq", "28 St", "23 St", "14 St - Union Sq", "8 St - NYU",
-    "Prince St", "Canal St", "City Hall", "Cortlandt St", "Rector St",
-    "Whitehall St - South Ferry", "Court St", "Jay St - MetroTech", 
-    "DeKalb Av", "Atlantic Av - Barclays Ctr", "36 St", "45 St", "53 St",
-    "59 St", "8 Av", "Fort Hamilton Pkwy", "New Utrecht Av", "18 Av", 
-    "20 Av", "Bay Pkwy", "Kings Hwy", "Avenue U", "86 St", "Coney Island - Stillwell Av"
+    "Astoria - Ditmars Blvd", 
+    "Astoria Blvd", 
+    "30 Av", 
+    "Broadway",
+    "36 Av", 
+    "39 Av - Dutch Kills", 
+    "Queensboro Plaza", 
+    "Lexington Av/59 St",
+    "5 Av/59 St", 
+    "57 St - 7 Av", 
+    "49 St", 
+    "Times Sq - 42 St",
+    "34 St - Herald Sq", 
+    "28 St", 
+    "23 St", 
+    "14 St - Union Sq", 
+    "8 St - NYU",
+    "Prince St", 
+    "Canal St", 
+    "City Hall", 
+    "Cortlandt St", 
+    "Rector St",
+    "Whitehall St - South Ferry", 
+    "Court St", 
+    "Jay St - MetroTech", 
+    "DeKalb Av", 
+    "Atlantic Av - Barclays Ctr", 
+    "36 St", 
+    "45 St", 
+    "53 St",
+    "59 St", 
+    "8 Av", 
+    "Fort Hamilton Pkwy", 
+    "New Utrecht Av", 
+    "18 Av", 
+    "20 Av", 
+    "Bay Pkwy", 
+    "Kings Hwy", 
+    "Avenue U", 
+    "86 St", 
+    "Coney Island - Stillwell Av"
 ]
 
 stations_D = [
-    "Norwood - 205 St", "Bedford Park Blvd", "Kingsbridge Rd", "Fordham Rd",
-    "182-183 Sts", "Tremont Av", "174-175 Sts", "170 St", "167 St", "161 St - Yankee Stadium",
-    "155 St", "145 St", "7 Av", "47-50 Sts - Rockefeller Ctr", "42 St - Bryant Pk",
-    "34 St - Herald Sq", "23 St", "14 St", "W 4 St - Washington Sq", "Broadway-Lafayette St",
-    "Grand St", "Atlantic Av - Barclays Ctr", "36 St", "9 Av", "Fort Hamilton Pkwy", 
-    "50 St", "55 St", "62 St", "71 St", "79 St", "18 Av", "20 Av", "Bay Pkwy", 
-    "25 Av", "Bay 50 St", "Coney Island - Stillwell Av"
+    "Norwood - 205 St", 
+    "Bedford Park Blvd", 
+    "Kingsbridge Rd", 
+    "Fordham Rd",
+    "182-183 Sts", 
+    "Tremont Av", 
+    "174-175 Sts", 
+    "170 St", 
+    "167 St", 
+    "161 St - Yankee Stadium",
+    "155 St", 
+    "145 St", 
+    "7 Av", 
+    "47-50 Sts - Rockefeller Ctr", 
+    "42 St - Bryant Pk",
+    "34 St - Herald Sq", 
+    "23 St", 
+    "14 St", 
+    "W 4 St - Washington Sq", 
+    "Broadway-Lafayette St",
+    "Grand St", 
+    "Atlantic Av - Barclays Ctr", 
+    "36 St", 
+    "9 Av", 
+    "Fort Hamilton Pkwy", 
+    "50 St", 
+    "55 St", 
+    "62 St", 
+    "71 St", 
+    "79 St", 
+    "18 Av", 
+    "20 Av", 
+    "Bay Pkwy", 
+    "25 Av", 
+    "Bay 50 St", 
+    "Coney Island - Stillwell Av"
 ]
 
 # Додавання станцій і ліній до графа
@@ -45,7 +141,7 @@ for line, stops in [('R', stations_R), ('N', stations_N), ('D', stations_D)]:
     for i in range(len(stops) - 1):
         G.add_edge(stops[i], stops[i + 1], line=line)
 
-# Функція для DFS
+# DFS
 def dfs(graph, start, goal, path=None):
     if path is None:
         path = []
@@ -59,7 +155,7 @@ def dfs(graph, start, goal, path=None):
                 return newpath
     return None
 
-# Функція для BFS
+# BFS
 def bfs(graph, start, goal):
     queue = deque([[start]])
     visited = set()
@@ -77,7 +173,7 @@ def bfs(graph, start, goal):
                 return new_path
     return None
 
-# Вхідні дані для алгоритмів
+# Вхідні дані для розрахунків
 start_station = "Forest Hills - 71 Av"
 goal_station = "Coney Island - Stillwell Av"
 
--- README.md
+++ README.md
@@ -3,16 +3,15 @@
 # Graphs
 
 ## Introduction
-This project implements and compares depth-first and breadth-first search over a graph with uniform edge weights.
+This project explores and compares depth-first search (DFS) and breadth-first search (BFS) algorithms on a graph representing the Metropolitan Transportation Authority's D, N, and R train lines in New York City. The graph has uniform edge weights, representing the transit network within the metropolitan area. The aim is to analyze and demonstrate the differences in pathfinding strategies between DFS and BFS on this public transportation system.
 
 ## Algorithms
 ### Depth-First Search (DFS). 
-The DFS algorithm searches deeper into the graph, exploring all adjacent stations before backtracking. This approach can result in finding a possible path that may be longer or less direct. It can also identify the deepest path to the goal, even if shorter paths exist.
+DFS dives deep into the graph by exploring each adjacent station fully before backtracking. This strategy may uncover a path that is longer or less direct. It can also identify the most deeply nested path to the destination, even when shorter routes are available.
 ### Breadth-First Search (BFS). 
-BFS explores all levels (i.e., stations one step away from the current station) before moving deeper. This method guarantees finding the shortest path when edges have uniform weights. BFS always identifies the shortest path in terms of the number of edges.
+BFS examines all stations at the current level before moving to the next level. This method ensures the shortest path is found when all edges have equal weights, as it always locates the path with the fewest edges.
 
 ## Comparison and Conclusions
-DFS and BFS found different paths, it is due to their distinct approaches. DFS may discover a longer path due to its deep search, while BFS will always find the shortest path given uniform edge weights.
-
+DFS and BFS identified different paths (23 and 18 respectively) due to their unique methods. DFS may find a longer route because it explores each path deeply before backtracking, while BFS always locates the shortest path, as it considers all paths at each level first.
 ### Conclusions
-BFS searches for the shortest path by the number of edges, making it optimal for finding the shortest routes. DFS, on the other hand, may opt for a longer path due to its deep exploration before branching out.
+BFS is optimal for finding the shortest path by the number of edges, making it ideal for identifying the most efficient routes. Conversely, DFS might choose a longer path as it explores deeper before considering alternative routes.
